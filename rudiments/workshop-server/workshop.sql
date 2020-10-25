e0fe42c2976434d96a4c9175f157ea021d76b41e
Brad Weems <brad.weems@bronji.com>
2020-10-25 18:36:15 -0400

    deleted second workshop sql file

--- rudiments/workshop-server/workshop.sql
+++ rudiments/workshop-server/workshop.sql
@@ -26,5 +26,32 @@ CREATE TABLE attendees (
 	attendee TEXT NOT NULL
 ); 
 
-SELECT attendees.id, attendees.attendee, workshop, workshop_id FROM attendees INNER JOIN workshops ON 
-attendees.workshop_id = workshops.id;
+
+***********this is the setup for the second version database*************
+CREATE DATABASE workshopserver2;
+
+CREATE TABLE workshops (
+  
+  workshop_id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  date date,
+  location TEXT NOT NULL,
+  maxSeats INTEGER,
+  instructor TEXT NOT NULL
+);
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    firstname TEXT NOT NULL,
+    lastname TEXT NOT NULL,
+    email TEXT NOT NULL
+);
+
+ 
+CREATE TABLE attendees (
+  
+  workshop_id INTEGER REFERENCES workshops(workshop_id),
+  user_id INTEGER REFERENCES users(user_id),
+  PRIMARY KEY (workshop_id, user_id) 
+); 
